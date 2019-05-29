36d7a32b706d9dc4d204a5f5054d666de15adbdc
fromthehart <fromthehart@pm.me>
2019-05-29 00:43:49 -0400

    (calculator) MVP complete

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
+  // Calculator button layout as an array
+  const calculatorButtons = [
+    ['clear', '÷'],
+    ['7', '8', '9', '×'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '=']
+  ]
+    
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app">
+      <CalculatorDisplay />
+      {
+        calculatorButtons.map(row => {
+          return (
+
+            // Iterate over each row of buttons as represented in the calculatorButtons array
+            row.map((button, index) => {
+
+              let style = 'normal';
+              // If a row only has two keys, the first button should be wide
+              if (row.length === 2 && index === 0) {
+                style = 'wide';
+              // If this is the last button in a row, make it red
+              } else if (index === row.length - 1) {
+                style = 'red';
+              }  
+
+              return (
+                '0123456789'.includes(button) ? // If the button has a number on it
+                  <NumberButton num={button} buttonStyle={style} />: // ... then output a NumberButton
+                  <ActionButton action={button} buttonStyle={style} /> // ... else output an ActionButton
+              )
+            })
+          )
+        })
+      }
     </div>
   );
 };
